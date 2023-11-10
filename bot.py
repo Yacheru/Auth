9633d6e8c696ced656e7c175bddc4db337b1eafa
Yacheru <amilohin274@gmail.com>
2023-11-10 09:47:45 +0300

    del useless code

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import json
 import discord
 import dotenv
-import os
 from discord.ext import commands
 
 with open('config.json', 'r', encoding='utf-8') as f:
@@ -13,14 +12,6 @@ bot = commands.Bot(
     activity=discord.Activity(
         type=2, name="https://yacheru.ru/login"),
     help_command=None)
-
-
-@bot.event
-async def on_ready():
-    for f in os.listdir("main/cogs"):
-        if f.endswith(".py"):
-            await bot.load_extension(f"cogs.{f[:-3]}")
-
 
 dotenv.load_dotenv()
 TOKEN = cfg['BOT']['TOKEN']
